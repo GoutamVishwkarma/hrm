5bae926a0c55c87a71c6dcf0600370cc9dc9eeac
kuldeep <kuldeeppanwar7697@gmail.com>
2023-07-09 19:01:08 +0530

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
-import { AllExceptionsFilter } from './helpers/filters/exception.filter';
 import {  Module, } from '@nestjs/common';
 import { DevtoolsModule } from '@nestjs/devtools-integration';
 import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from 'src/app.controller';
 import { DatabaseModule } from 'src/modules/database';
 import { AuthModule } from 'src/modules/auth';
 import { UsersModule } from 'src/modules/users';
-import { TransformResponseInterceptor } from './helpers/interceptor';
 @Module({
   imports: [  DevtoolsModule.register({
     http: process.env.NODE_ENV !== 'production',
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, Logger, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {  Injectable, Logger } from '@nestjs/common';
 import { User } from 'src/modules/database/models';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -22,7 +22,6 @@ export class UsersService {
     return this.userModel.findOne({userId:id}).exec();
   }
   async findOneByEmail(email: string): Promise<User> {
-    throw new UnauthorizedException("Not found");
     return await this.userModel.findOne({ email }).populate('roles').exec();
   }
 
